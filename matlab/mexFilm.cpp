3ca1ad8a43af4dc59b79e5b34a7e9557406c51b3
Mark <mah54746929@qq.com>
2021-05-11 16:42:57 +0200

    add MATLAB

---
+++ matlab/mexFilm.cpp
@@ -0,0 +1,59 @@
+#include "mex.h"
+#include "multilayer.cpp"
+
+void mexFunction(int nlhs, mxArray *plhs[],int nrhs,const mxArray *prhs[])
+{
+    double *inMatrix_n;               /* real part of index 1xN input matrix */
+    double *inMatrix_k;               /* imagnary part of 1xN input matrix */
+    double *inMatrix_thickness;       /* thickness of 1xN input matrix */
+    double *outMatrix;              /* output matrix */
+    
+    mexPrintf("the input accepts as: \nint NumberLayers, array n, array k, array thickness, double angleIncidence(deg), double wavelength(nm), bool P(true)_S(false)\n");
+    
+    /* Check for proper number of input and output arguments */    
+    if (nrhs != 7) {
+        mexErrMsgIdAndTxt( "MATLAB:mxcreatecellmatrix:minrhs",
+                "the input must be int NumberLayers, array n, array k, array thickness, double angleIncidence(deg), double wavelength(nm),bool P(true)_S(false)\n");
+    } 
+    if(nlhs > 1){
+        mexErrMsgIdAndTxt( "MATLAB:mxcreatecellmatrix:maxlhs",
+                "Too many output arguments.");
+    }
+    
+    /* get the number of the layers  */
+    int numofLayers = mxGetScalar(prhs[0]);
+    
+    /* create a pointer to the real data in the input matrix  */
+    inMatrix_n = mxGetPr(prhs[1]);
+    inMatrix_k = mxGetPr(prhs[2]);
+    inMatrix_thickness = mxGetPr(prhs[3]);
+    
+    // size of n and k array
+    int ncols_n = mxGetN(prhs[1]);
+    int ncols_k = mxGetN(prhs[2]);
+    int ncols_thickness = mxGetN(prhs[3]);
+    
+    for (int i0=0;i0<ncols_n;i0++)
+    {
+        mexPrintf("%.2f\t%.2f\t%.2f\t\n",inMatrix_n[i0],inMatrix_k[i0],inMatrix_thickness[i0]);
+    }
+    
+    //
+    double angleofIncidence = mxGetScalar(prhs[4]);
+    double wavelength = mxGetScalar(prhs[5]);
+    bool SPindicator = mxGetScalar(prhs[6]);
+    
+    if (ncols_n!=ncols_k ||ncols_n!=ncols_thickness)
+    {
+        mexErrMsgIdAndTxt( "MATLAB:mxcreatecellmatrix:minrhs",
+                "the input arraies are not equal.");
+    }
+    
+    plhs[0] = mxCreateDoubleMatrix(1,1,mxREAL);
+    outMatrix = mxGetPr(plhs[0]);
+    
+    outMatrix[0] = calculateReflectivity(numofLayers, inMatrix_n,inMatrix_k, inMatrix_thickness, angleofIncidence, wavelength, SPindicator);
+    
+    
+    
+}
---
+++ matlab/test.m
@@ -0,0 +1,32 @@
+Num = 70
+reflection_S = zeros(Num,1)
+reflection_P = zeros(Num,1)
+
+angles = [1:1:Num]
+
+% glass, Film (100 nm), glass.
+% angle from 1 to Num (deg).
+% wavelength 525 nm.
+% the last variable, true means P polarization, false for S.
+
+n_index = [1.5194,1.51,1.5194]
+k_index = [0., 0., 0.]
+thickness = [0,100,0]
+
+for i0=1:Num
+    reflection_S(i0,1) = mexFilm(3,n_index, k_index, thickness,i0,525.0,false)
+    reflection_P(i0,1) = mexFilm(3,n_index, k_index, thickness,i0,525.0,true)
+end
+
+% read 
+x_s=csvread('2um_525nm_S_macleod.csv',1,0)
+x_p=csvread('2um_525nm_P_macleod.csv',1,0)
+
+plot(angles, reflection_S*100,'b')
+hold on
+plot(angles, reflection_P*100,'g')
+plot(x_s(:,1), x_s(:,2),'b.')
+plot(x_p(:,1), x_p(:,2),'g.')
+xlim([8,71])
+hold off
+legend('mex S','mex P','macleod S','macloed P')
