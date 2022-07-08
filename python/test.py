34a4385cff0909fa6805fb582c2a41e163236cf2
hm <mah54746929"qq.com>
2022-07-08 11:21:32 +0200

    change test file

--- python/test.py
+++ python/test.py
@@ -13,6 +13,11 @@ wavelength = np.float64(632)
 
 print ("Angle\tresult")
 
+
+resR_p_all = np.array([])
+resR_s_all = np.array([])
+resT_p_all = np.array([])
+resT_s_all = np.array([])
 for i0 in range(90):
     resR_p = x.calReflectivity(N,n,k,h,np.float64(i0),wavelength,1) # p-polarise
 
@@ -21,3 +26,22 @@ for i0 in range(90):
     resT_p = x.calTransmission(N,n,k,h,np.float64(i0),wavelength,1) # p-polarise
     resT_s = x.calTransmission(N,n,k,h,np.float64(i0),wavelength,0) # p-polarise
     print ("i0: {}, R_s: {},R_p: {}, T_p: {},T_s: {}".format(i0, resR_p, resR_s, resT_p, resT_s))
+
+    resR_p_all = np.append(resR_p_all, resR_p)
+    resR_s_all = np.append(resR_s_all, resR_s)
+    resT_p_all = np.append(resT_p_all, resT_p)
+    resT_s_all = np.append(resT_s_all, resT_s)
+
+import matplotlib.pyplot as plt
+
+plt.figure(dpi=150)
+plt.plot(np.linspace(0,90,90),resR_p_all, label='R_p')
+plt.plot(np.linspace(0,90,90),resR_s_all, label='R_s')
+plt.plot(np.linspace(0,90,90),resT_p_all, label='T_p')
+plt.plot(np.linspace(0,90,90),resT_s_all, label='T_s')
+plt.legend()
+plt.tight_layout(pad=0.5)
+plt.savefig("results")
+plt.close()
+
+
